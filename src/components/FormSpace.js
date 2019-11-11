cc2c99cf8c8434ce812687493e04f414da9eb373
samfauble <53917130+samfauble@users.noreply.github.com>
2019-11-11 19:39:28 -0400

    brought form component elements to same place

--- src/components/FormSpace.js
+++ src/components/FormSpace.js
@@ -1,8 +1,5 @@
 import React, { useState } from 'react'
-import Attendance from "./Attendance"
-import NumberAttending from "./NumberAttending"
-import SelectDishType from "./SelectDishType"
-import Submit from "./Submit"
+
 
 function FormSpace() {
     //State Hooks
@@ -10,15 +7,54 @@ function FormSpace() {
     const [guestNumber, setGuestNumber] = useState();
     const [dishType, setDishType] = useState();
     
-    
     const displayAnswers = (event) => {}
 
     return (
         <form>
-          <Attendance value={isGoing} onChange={setIsGoing}/>
-          <NumberAttending value={guestNumber} onChange={setGuestNumber} />
-          <SelectDishType value={dishType} onChange={setDishType} />
-          <Submit onSubmit={displayAnswers} />
+            <div>
+                Are you attending?
+                <div>
+                    <input type="radio" id="yes" name="yesNo" value="yes" />
+                    <label htmlFor="yes">Yes</label>
+                </div>
+                <div>
+                    <input type="radio" id="no" name="yesNo" />
+                    <label htmlFor="no">No</label>
+                </div>
+            </div>
+            <div>
+            How many will be in attendance?
+                <div>
+                    <input type="radio" id="one" name="number"/>
+                    <label htmlFor="one">One person</label>
+                </div>
+                <div>
+                    <input type="radio" id="two" name="number"/>
+                    <label htmlFor="two">Two people</label>
+                </div>
+                <div>
+                    <input type="radio" id="three" name="number"/>
+                    <label htmlFor="three">Three people</label>
+                </div>
+                <div>
+                    <input type="radio" id="four" name="number"/>
+                    <label htmlFor="four">Four people</label>
+                </div>
+                <div>
+                    <input type="radio" id="five" name="number"/>
+                    <label htmlFor="five">Five people</label>
+                </div>
+            </div>
+            <div>
+                What would you like as an entree?
+                <div>
+                    <select>
+                        <option value="fish">Fish</option>
+                        <option value="steak">Steak</option>
+                        <option value="pasta">Pasta</option>
+                    </select>
+                </div>
+            </div>
         </form>
     )
 }
