4d93142fc365902ac55bc0d624201d7e9f24ce42
samfauble <53917130+samfauble@users.noreply.github.com>
2019-11-11 19:49:54 -0400

    refactored FormSpace as a class component

--- src/components/FormSpace.js
+++ src/components/FormSpace.js
@@ -1,62 +1,70 @@
 import React, { useState } from 'react'
+import { ReactComponent } from '*.svg'
 
 
-function FormSpace() {
-    //State Hooks
-    const [isGoing, setIsGoing] = useState();
-    const [guestNumber, setGuestNumber] = useState();
-    const [dishType, setDishType] = useState();
+class FormSpace extends React.Component {
+
+    constructor(props) {
+        super(props)
     
-    const displayAnswers = (event) => {}
+        this.state = {
+             isGoing: false,
+             guestNumber: 0,
+             dishType: []
+        }
+    }
+    
 
-    return (
-        <form>
-            <div>
-                Are you attending?
+    render(){
+        return (
+            <form>
                 <div>
-                    <input type="radio" id="yes" name="yesNo" value="yes" />
-                    <label htmlFor="yes">Yes</label>
+                    Are you attending?
+                    <div>
+                        <input type="radio" id="yes" name="yesNo" value="yes" />
+                        <label htmlFor="yes">Yes</label>
+                    </div>
+                    <div>
+                        <input type="radio" id="no" name="yesNo" />
+                        <label htmlFor="no">No</label>
+                    </div>
                 </div>
                 <div>
-                    <input type="radio" id="no" name="yesNo" />
-                    <label htmlFor="no">No</label>
+                How many will be in attendance?
+                    <div>
+                        <input type="radio" id="one" name="number"/>
+                        <label htmlFor="one">One person</label>
+                    </div>
+                    <div>
+                        <input type="radio" id="two" name="number"/>
+                        <label htmlFor="two">Two people</label>
+                    </div>
+                    <div>
+                        <input type="radio" id="three" name="number"/>
+                        <label htmlFor="three">Three people</label>
+                    </div>
+                    <div>
+                        <input type="radio" id="four" name="number"/>
+                        <label htmlFor="four">Four people</label>
+                    </div>
+                    <div>
+                        <input type="radio" id="five" name="number"/>
+                        <label htmlFor="five">Five people</label>
+                    </div>
                 </div>
-            </div>
-            <div>
-            How many will be in attendance?
                 <div>
-                    <input type="radio" id="one" name="number"/>
-                    <label htmlFor="one">One person</label>
+                    What would you like as an entree?
+                    <div>
+                        <select>
+                            <option value="fish">Fish</option>
+                            <option value="steak">Steak</option>
+                            <option value="pasta">Pasta</option>
+                        </select>
+                    </div>
                 </div>
-                <div>
-                    <input type="radio" id="two" name="number"/>
-                    <label htmlFor="two">Two people</label>
-                </div>
-                <div>
-                    <input type="radio" id="three" name="number"/>
-                    <label htmlFor="three">Three people</label>
-                </div>
-                <div>
-                    <input type="radio" id="four" name="number"/>
-                    <label htmlFor="four">Four people</label>
-                </div>
-                <div>
-                    <input type="radio" id="five" name="number"/>
-                    <label htmlFor="five">Five people</label>
-                </div>
-            </div>
-            <div>
-                What would you like as an entree?
-                <div>
-                    <select>
-                        <option value="fish">Fish</option>
-                        <option value="steak">Steak</option>
-                        <option value="pasta">Pasta</option>
-                    </select>
-                </div>
-            </div>
-        </form>
-    )
+            </form>
+        );
+    }
 }
 
 export default FormSpace
