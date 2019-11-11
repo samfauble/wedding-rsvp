92c9d8608469e404918450443cc97fe0ce03bea5
samfauble <53917130+samfauble@users.noreply.github.com>
2019-11-11 18:42:10 -0400

    state templates revised

--- src/components/FormSpace.js
+++ src/components/FormSpace.js
@@ -5,15 +5,20 @@ import SelectDishType from "./SelectDishType"
 import Submit from "./Submit"
 
 function FormSpace() {
+    //State Hooks
     const [isGoing, setIsGoing] = useState();
     const [guestNumber, setGuestNumber] = useState();
     const [dishType, setDishType] = useState();
+    
+    
+    const displayAnswers = (event) => {}
+
     return (
         <form>
           <Attendance value={isGoing} onChange={setIsGoing}/>
           <NumberAttending value={guestNumber} onChange={setGuestNumber} />
           <SelectDishType value={dishType} onChange={setDishType} />
-          <Submit />
+          <Submit onSubmit={displayAnswers} />
         </form>
     )
 }
