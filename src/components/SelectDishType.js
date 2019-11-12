b8c86c44f623dcf34765faf8e5b7ec4b40501cb7
samfauble <53917130+samfauble@users.noreply.github.com>
2019-11-12 15:14:21 -0400

    created multiple SelectDishType selectors

--- src/components/SelectDishType.js
+++ src/components/SelectDishType.js
@@ -1,14 +1,26 @@
 import React from 'react'
 
+function CreateListItem(props){
+    return(
+        <select>
+            <option value="fish">Fish</option>
+            <option value="steak">Steak</option>
+            <option value="pasta">Pasta</option>
+        </select>
+    )
+}
+
+
 function SelectDishType(props) {
+    const rows = [];
+    const number = props.number;
+    for (let i=0; i<=number-1; i++){
+        rows.push(<CreateListItem key={"item"+i} />)
+    }
     return (
         <div name="food" multiple={true} value={props.value} onChange={props.setMeal} className={props.isGoing==="yes" ? "show" : "hide"}>
             What would you like as a main course? <br/>
-            <select>
-                <option value="fish">Fish</option>
-                <option value="steak">Steak</option>
-                <option value="pasta">Pasta</option>
-            </select>
+            {rows}
         </div>
     )
 }
